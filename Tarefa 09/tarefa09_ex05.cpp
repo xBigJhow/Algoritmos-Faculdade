871d2f9c465e3cb4bbf73f408b8cdbbd84655384
Jonatas Freitas <103526432+xBigJhow@users.noreply.github.com>
2022-10-23 16:48:54 -0300

    Tarefa 09 - Algoritmos

---
+++ Tarefa 09/tarefa09_ex05.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
+#define qtd 100
+struct data_nascimento {
+	int Dia, Mes, Ano;
+};
+struct dados_funcionario {
+	int idade, setor;
+	char nome[50], cpf[11], sexo, cargo[30];
+	float salario;
+	struct data_nascimento Data;
+};
+
+void imprimirFunc(int i, struct dados_funcionario Funcionario) {
+	printf("------ Funcionário %d ------\n", i);
+	printf("Nome: %s \nCPF: %s \nIdade: %i\n", Funcionario.nome, Funcionario.cpf, Funcionario.idade);
+	printf("Sexo: %c\nSetor: %d\nCargo: %s\n", Funcionario.sexo, Funcionario.setor, Funcionario.cargo);
+	printf("Salário: R$%.2f\n", Funcionario.salario);
+	printf("Data Nascimento: %d/%d/%d\n", Funcionario.Data.Dia, Funcionario.Data.Mes, Funcionario.Data.Ano);	
+}
+
+void cadastraFunc(int i, struct dados_funcionario &Funcionario) {
+		printf("<:--- INSIRA OS DADOS DO FUNCIONARIO ---:>\n");
+		printf("%d° Funcionario\n", i+1);
+		printf("Nome: ");
+		fflush(stdin);
+		gets(Funcionario.nome);
+		printf("CPF: ");
+		fflush(stdin);
+		fgets(Funcionario.cpf, 11, stdin);
+		printf("Idade: ");
+		scanf("%d", &Funcionario.idade);
+		printf("Sexo [M] Masculino [F] Feminino: ");
+		fflush(stdin);
+		scanf("%c", &Funcionario.sexo);
+		printf("Setor onde trabalha [0 - 99]: ");
+		scanf("%d", &Funcionario.setor);
+		printf("Cargo: ");
+		fflush(stdin);
+		gets(Funcionario.cargo);
+		printf("Salário: R$");
+		scanf("%f", &Funcionario.salario);
+		printf("Data de nascimento:\n");
+		printf("Dia: ");
+		scanf("%d", &Funcionario.Data.Dia);
+		printf("Mes: ");
+		scanf("%d", &Funcionario.Data.Mes);
+		printf("Ano: ");
+		scanf("%d", &Funcionario.Data.Ano);
+		system("cls");
+}
+
+int main() {
+	setlocale(LC_ALL, "Portuguese"); // pt-br
+	struct dados_funcionario Funcionario[qtd];
+	int menu, cadastrados=0;
+	do {
+		printf("=-=-=-=- MENU -=-=-=-=\n");
+		printf("[1] Cadastrar Funcionario\n[2] Listar Funcionarios \n");
+		printf("[3] Sair\nEscolha: ");
+		scanf("%d", &menu);
+		system("cls");
+		switch(menu) {
+			case 1:
+				cadastraFunc(cadastrados, Funcionario[cadastrados]);
+				cadastrados++;
+				break;
+			case 2:
+				for(int i=0; i<cadastrados; i++) {
+					imprimirFunc(i, Funcionario[i]);
+				}
+				break;
+			case 3:
+				// para o programa!
+				printf("Volte Sempre!");
+				cadastrados = 100;
+				break;
+			default:
+				printf(">> OPÇÃO INCORRETA! <<");
+				break;	
+		}
+	} while	(cadastrados < qtd);	
+	return 0;
+}
