12a3b21b5c372abcbae696bfd60f752232f3d032
Jonatas Freitas <103526432+xBigJhow@users.noreply.github.com>
2022-10-27 19:07:45 -0300

    Update tarefa09_ex09.cpp

--- Tarefa 09/tarefa09_ex09.cpp
+++ Tarefa 09/tarefa09_ex09.cpp
@@ -1,15 +1,15 @@
-/* 9. Faça um programa que controle o fluxo de voos nos aeroportos de um país. Com V=5 (voos) e A=5 (aeroportos) e: 
-a. Crie e leia um vetor de voos, sendo que cada voo contém um código de aeroporto de origem e um de destino. 
-b. Crie um vetor de aeroportos, sendo que cada aeroporto contém seu código, quantidade de voos que saem e quantidade de voos que chegam. 
+/* 9. FaÃ§a um programa que controle o fluxo de voos nos aeroportos de um paÃ­s. Com V=5 (voos) e A=5 (aeroportos) e: 
+a. Crie e leia um vetor de voos, sendo que cada voo contÃ©m um cÃ³digo de aeroporto de origem e um de destino. 
+b. Crie um vetor de aeroportos, sendo que cada aeroporto contÃ©m seu cÃ³digo, quantidade de voos que saem e quantidade de voos que chegam. 
 
-Nota: Cada aeroporto é identificado por um código inteiro entre 0 e (A-1). Não aceite aeroportos de código inexistente. 
+Nota: Cada aeroporto Ã© identificado por um cÃ³digo inteiro entre 0 e (A-1). NÃ£o aceite aeroportos de cÃ³digo inexistente. 
 */
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
 #include <stdbool.h>
 #include <windows.h>
-#define QTD 2
+#define QTD 5
 
 
 struct dados_voo {
@@ -25,8 +25,8 @@ struct aeroportos {
 };
 
 void cadastraAeroporto(struct aeroportos &Aeroporto, int i) {
-    printf("=-=-=-=-=- CADASTRANDO %d° AEROPORTO -=-=-=-=-=-=\n", i);
-    printf("Código do Aeroporto [5 digitos]: ");
+    printf("=-=-=-=-=- CADASTRANDO %dÂ° AEROPORTO -=-=-=-=-=-=\n", i);
+    printf("CÃ³digo do Aeroporto [5 digitos]: ");
     scanf("%d", &Aeroporto.codigo);
     printf("Cidade do Aeroporto: ");
     fflush(stdin);
@@ -45,17 +45,17 @@ int main() {
     struct aeroportos Aeroporto[QTD]; //estrutura
     struct dados_voo Voo;
     char maisVoos = 's'; // variavel para continuar adicionando voos 
-    int contagemVoo = 1; // só para contar os voos
+    int contagemVoo = 1; // sÃ³ para contar os voos
     //cadastrando os aeroportos primeiros
     for(int i=0; i<QTD; i++) {
         cadastraAeroporto(Aeroporto[i], i+1);
     }
-    // irá inserir voos até parar!
+    // irÃ¡ inserir voos atÃ© parar!
     do {
     	bool correto_origem = false, correto_destino = false;
     	system("cls");
     	int x, y = 0;
-    	printf("=-=-=-=- %d° Voo -=-=-=-=\n\n", contagemVoo);
+    	printf("=-=-=-=- %dÂ° Voo -=-=-=-=\n\n", contagemVoo);
     	printf("Aeroporto de Origem: ");
     	scanf("%d", &Voo.origem);
     	printf("Aeroporto de Destino: ");
@@ -77,14 +77,14 @@ int main() {
 			}
 		}
     	if(correto_origem && correto_destino) {
-    		printf("Voo de n° %d cadastrado com sucesso!\n", contagemVoo);
-    		printf("Sairá de %s para %s\n", Aeroporto[x].endereco, Aeroporto[y].endereco);
+    		printf("Voo de nÂ° %d cadastrado com sucesso!\n", contagemVoo);
+    		printf("SairÃ¡ de %s para %s\n", Aeroporto[x].endereco, Aeroporto[y].endereco);
     		contagemVoo++;
 		} else {
 			printf("Favor conferir os dados inseridos: \n");
 		}
 		fflush(stdin);
-		printf("\n\nDeseja continuar [S] Sim [N] Não\nEscolha: ");
+		printf("\n\nDeseja continuar [S] Sim [N] NÃ£o\nEscolha: ");
 		scanf("%c", &maisVoos);
 	} while(maisVoos == 's' || maisVoos == 'S');
 	// imprimindo os levantamentos do aeroporto
